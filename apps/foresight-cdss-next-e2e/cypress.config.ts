02baca97f353733f69e2c4e87fdcbc5d279ade53
Michael Dadi <13662053+michaeldadi@users.noreply.github.com>
2025-08-27 17:02:15 -0400

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/01Zk2XGSHX

---
+++ apps/foresight-cdss-next-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run @foresight-cdss-next/foresight-cdss-next:dev',
+      },
+      ciWebServerCommand:
+        'npx nx run @foresight-cdss-next/foresight-cdss-next:start',
+      ciBaseUrl: 'http://localhost:3000',
+    }),
+    baseUrl: 'http://127.0.0.1:3000',
+  },
+});
