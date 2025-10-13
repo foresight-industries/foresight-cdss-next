e1b2818ff4484b917d1dd8285a22794fa762c1f7
Michael Dadi <13662053+michaeldadi@users.noreply.github.com>
2025-10-12 21:34:37 -0400

    Add infra CI deploy action

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,4 +1,21 @@
 import type { Config } from 'drizzle-kit';
+import { config } from '@dotenvx/dotenvx';
+
+config({
+  path: '.env.local',
+});
+
+if (!process.env.DATABASE_NAME) {
+  throw new Error('DATABASE_NAME is not defined');
+}
+
+if (!process.env.DATABASE_CLUSTER_ARN) {
+  throw new Error('DATABASE_CLUSTER_ARN is not defined');
+}
+
+if (!process.env.DATABASE_SECRET_ARN) {
+  throw new Error('DATABASE_SECRET_ARN is not defined');
+}
 
 export default {
   schema: './src/schema.ts',
@@ -6,8 +23,8 @@ export default {
   dialect: 'postgresql',
   driver: 'aws-data-api',
   dbCredentials: {
-    database: process.env.DATABASE_NAME!,
-    resourceArn: process.env.DATABASE_CLUSTER_ARN!,
-    secretArn: process.env.DATABASE_SECRET_ARN!,
+    database: process.env.DATABASE_NAME,
+    resourceArn: process.env.DATABASE_CLUSTER_ARN,
+    secretArn: process.env.DATABASE_SECRET_ARN,
   },
 } satisfies Config;
